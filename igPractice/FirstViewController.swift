5b202d357ffd0e518e2c177cf40766ba62346721
Derek <stiau334@gmail.com>
2018-08-30 23:04:13 +0800

    new

--- igPractice/FirstViewController.swift
+++ igPractice/FirstViewController.swift
@@ -8,10 +8,27 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
-
+class FirstViewController: UIViewController, UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        
+    }
+    
+   
     override func viewDidLoad() {
         super.viewDidLoad()
+        navigationController?.navigationBar.prefersLargeTitles = false
+        let searchController = UISearchController(searchResultsController: nil)
+        searchController.searchResultsUpdater = self
+        searchController.dimsBackgroundDuringPresentation = false
+        navigationItem.searchController = searchController
+        definesPresentationContext = true
+        navigationItem.hidesSearchBarWhenScrolling = false
+        searchController.searchBar.barStyle = .default
+        searchController.searchBar.barTintColor = .black
+        searchController.searchBar.sizeToFit()
+        let image = UIImage()
+        self.navigationController?.navigationBar.setBackgroundImage(image, for: .default)
+        self.navigationController?.navigationBar.shadowImage = image
         // Do any additional setup after loading the view, typically from a nib.
     }
 

eddd543e16e27bc14ada8b6770d6c4297e11fd7a
Derek <stiau334@gmail.com>
2018-08-30 19:35:51 +0800

    Initial Commit

---
+++ igPractice/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  igPractice
+//
+//  Created by Derek on 2018/8/30.
+//  Copyright © 2018年 Derek. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
